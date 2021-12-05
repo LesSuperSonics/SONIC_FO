d534c42d4587444325db37256deaa4116f34ba74
Younes OUFRID <younes.oufrid@hightech.edu>
2021-12-05 23:11:15 +0100

    ✅ Creating Auth , User , Token storage services TESTS
    ✅ Creating Auth , User , Token storage services TESTS

--- src/app/_services/auth.service.spec.ts
+++ src/app/_services/auth.service.spec.ts
@@ -1,16 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule} from '@angular/common/http/testing';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
-  let service: AuthService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [AuthService]
+  }));
 
-  it('should be created', () => {
+   it('should be created', () => {
+    const service: AuthService = TestBed.get(AuthService);
     expect(service).toBeTruthy();
-  });
+   });
+
 });
