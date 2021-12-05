8a26fdf6d830bc5813a41e010201b5f4353d7aef
Younes OUFRID <younes.oufrid@hightech.edu>
2021-12-05 23:11:15 +0100

    Revert "✅ Creating Auth , User , Token storage services TESTS"
    This reverts commit d534c42d4587444325db37256deaa4116f34ba74.

--- src/app/_services/auth.service.spec.ts
+++ src/app/_services/auth.service.spec.ts
@@ -1,17 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule} from '@angular/common/http/testing';
+
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
+  let service: AuthService;
 
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientTestingModule], 
-    providers: [AuthService]
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AuthService);
+  });
 
-   it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
+  it('should be created', () => {
     expect(service).toBeTruthy();
-   });
-
+  });
 });
