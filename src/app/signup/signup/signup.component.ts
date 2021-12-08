5854248e510175d2def02f74867ef4416b3cca6c
salahhalkhoums <halkhoumssalah@gmail.com>
2021-12-08 12:52:17 +0100

    commit to fix validation forms in signup and login

--- src/app/signup/signup/signup.component.ts
+++ src/app/signup/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../_services/auth.service';
 
@@ -12,33 +13,72 @@ export class SignupComponent implements OnInit {
     firstName: null,
     lastName: null,
     email: null,
-    password: null
+    password: null,
+    confirmPassword: null
   };
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
-  constructor(private router: Router,private authService: AuthService) {}
+
+
+  firstnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20)
+  ]);
+  lastnameFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(20)
+  ]);
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6)
+  ]);
+  confirmPasswordFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  constructor(private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit(): void {
-    const { firstName,lastName, email, password } = this.form;
-
-    this.authService.register(firstName,lastName, email, password).subscribe(
-      data => {
-        console.log(data);
-        this.isSuccessful = true;
-        this.isSignUpFailed = false;
-        this.router.navigate(['/login']);
-      },
-      err => {
-        this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
-      }
-    );
+    if (!this.firstnameFormControl.invalid &&
+      !this.lastnameFormControl.invalid &&
+      !this.emailFormControl.invalid &&
+      !this.passwordFormControl.invalid &&
+      !this.confirmPasswordFormControl.invalid) {
+      const { firstName, lastName, email, password } = this.form;
+      this.authService.register(firstName, lastName, email, password).subscribe(
+        data => {
+          console.log(data);
+          this.isSuccessful = true;
+          this.isSignUpFailed = false;
+          this.router.navigate(['/login']);
+        },
+        err => {
+          this.errorMessage = err.error.message;
+          this.isSignUpFailed = true;
+        }
+      );
+    }
   }
-  onLogin(){
+  onLogin() {
     this.router.navigate(['/login']);
+  }
+
+  onConfirmPasswordChange() {
+    if (this.form.password === this.form.confirmPassword) {
+      this.confirmPasswordFormControl.setErrors(null);
+    }
+    else {
+      this.confirmPasswordFormControl.setErrors({ match: true })
+    }
   }
 }
