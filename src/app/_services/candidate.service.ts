119974e26c6b5176258d5332b67b70335744dc48
ilyas <ilyas.agraou@capgemini.com>
2021-12-15 17:09:41 +0100

    🍱 performing dashboard display values 🍱

--- src/app/_services/candidate.service.ts
+++ src/app/_services/candidate.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { TokenStorageService } from './token-storage.service';
 
 
 
@@ -10,10 +11,48 @@ import { environment } from 'src/environments/environment';
 })
 export class CandidateService {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http : HttpClient,  private tokenStorageService: TokenStorageService) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.tokenStorageService.getToken()
+    })
+};
+
 
   getAllCandidates(){
     return this.http.get(`${environment.apiUrl}/candidates`)
    
   }
+
+// profiles
+
+  getFullStack(){
+    return this.http.get(`${environment.apiUrl}/candidates/fullstackCount`)
+  }
+
+  getTesting(){
+    return this.http.get(`${environment.apiUrl}/candidates/testingCount`)
+  }
+
+  getSaleForce(){
+    return this.http.get(`${environment.apiUrl}/candidates/salesforceCount`)
+  }
+
+  // Status
+
+  getAccepted(){
+    return this.http.get(`${environment.apiUrl}/candidates/AcceptedStatusCount`)
+  }
+
+  getCurrent(){
+    return this.http.get(`${environment.apiUrl}/candidates/CurrentStatusCount`)
+  }
+
+  getRejected(){
+    return this.http.get(`${environment.apiUrl}/candidates/RejectedStatusCount`)
+  }
+
+
 }
--- src/app/admin/dashboard/home/home.component.ts
+++ src/app/admin/dashboard/home/home.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { CandidateService } from 'src/app/_services/candidate.service';
 import { UserService } from '../../../_services/user.service';
+
 interface Place {
   imgSrc: string;
   name: string;
@@ -14,10 +17,96 @@ interface Place {
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+ 
+  countFS:number=0;
+  labelFS:string="";
+
+  countTS : number=0;
+  labelTS : string="";
+
+  countSF : number=0;
+  labelSF : string="";
+
+  countCR : number=0;
+  labelCR : string="";
+
+  countAC : number=0;
+  labelAC : string="";
+
+  countRJ : number=0;
+  labelRJ : string="";
+
   places: Array<Place> = [];
   content?: string;
-  constructor(private userService: UserService) {}
+  constructor(private router : Router, private candidateService : CandidateService) {}
+
   ngOnInit() {
+    this.getFullStack();
+    this.getTesting();
+    this.getSalesForce();
+
+    this.getCurrent();
+    this.getAccepted();
+    this.getRejected();
+  }
+
+
+    getFullStack(){
+      this.candidateService.getFullStack().subscribe(
+        res => {
+          this.labelFS = "FullStack",
+          this.countFS = JSON.parse(JSON.stringify(res))['Total_FullStack']
+          //console.log(this.label + this.count)
+        });
+      }
+
+      getTesting(){
+        this.candidateService.getTesting().subscribe(
+          res => {
+            this.labelTS = "Testing",
+            this.countTS = JSON.parse(JSON.stringify(res))['Total_Testing']
+            
+          });
+        }
+
+      getSalesForce(){
+        this.candidateService.getSaleForce().subscribe(
+          res => {
+            this.labelSF = "SalesForce",
+            this.countSF = JSON.parse(JSON.stringify(res))['Total_SalesForce']
+            
+          });
+      }
+
+      getCurrent(){
+        this.candidateService.getCurrent().subscribe(
+          res =>{
+              this.labelCR = "Current",
+              this.countCR = JSON.parse(JSON.stringify(res))['Total_Current']
+        });
+        
+      }
+
+
+      getAccepted(){
+        this.candidateService.getAccepted().subscribe(
+          res =>{
+              this.labelAC = "Accepted",
+              this.countAC = JSON.parse(JSON.stringify(res))['Total_Accepted']
+        });
+        
+      }
+
+      getRejected(){
+        this.candidateService.getRejected().subscribe(
+          res =>{
+              this.labelRJ = "Rejected",
+              this.countRJ = JSON.parse(JSON.stringify(res))['Total_Rejected']
+        });
+        
+      }
+
+
     // give access to the scope with authorized resource to use later with users roles 
     /*this.userService.getAdminBoard().subscribe(
       data => {
@@ -28,5 +117,7 @@ export class HomeComponent implements OnInit {
       }
     );*/
     
-  }
+  
+
+  
 }
