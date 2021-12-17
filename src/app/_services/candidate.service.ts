5666e159197ee5171399f668049909932db46838
Younes OUFRID <younes.oufrid@hightech.edu>
2021-12-17 23:03:04 +0100

    🚀 adding change status functions to consume service
    🚀 adding change status functions to consume service

--- src/app/_services/candidate.service.ts
+++ src/app/_services/candidate.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Candidate } from '../models/candidate';
